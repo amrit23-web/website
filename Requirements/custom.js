3270dd237b68494181c531e3123ba3ecd5c02eab
Amritanshu Sharma <98414882+amrit23-web@users.noreply.github.com>
2023-09-01 21:34:32 +0530

    Add files via upload

---
+++ Requirements/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // Your code goes here
+});
+
+
+
+
+$(window).load(function() {
+  if (screen.width <= 699) {
+    window.location.href = "https://amrit23-web.github.io/website/";
+  }
+});
